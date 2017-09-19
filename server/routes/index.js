5987804bb2f65d17c131ec5a19d51dc6519a20d0
Yit Chun <yho@tremorvideo.com>
2017-09-19 12:33:18 +0800

    Created all endpoints in routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,62 @@ const errors = require('restify-errors')
 const GiftExchange = require('../app/index')
 
 module.exports = function (server, connectionPool) {
-    /**
-     * GET
-     */
+  /**
+    * Test Endpoint
+    */
   server.get('/exchange/test', (req, res, next) => {
     GiftExchange.test(res, connectionPool)
+  })
+
+  /**
+  * Exchange Endpoints
+  */
+  // Generate sender/reciever pairs and save to db
+  server.post('/exchange/generate', (req, res, next) => {
+
+  })
+
+  // send out email
+  server.post('/exchange/email', (req, res, next) => {
+
+  })
+
+  // get event status
+  server.get('/exchange/status/:eventId', (req, res, next) => {
+    let eventId = req.params.eventId
+  })
+
+  /**
+  * Wishlist Endpoints
+  */
+  // Save wishlist
+  server.post('wishlist/:idHash', (req, res, next) => {
+    let idHash = req.params.idHash
+  })
+
+  // Read wishlist
+  server.get('wishlist/:idHash', (req, res, next) => {
+    let idHash = req.params.idHash
+  })
+
+  /**
+  * Event Endpoints
+  */
+  // Create Event
+  server.post('event', (req, res, next) => {
+
+  })
+
+  /**
+  * User Endpoints
+  */
+  // Create user
+  server.post('user', (req, res, next) => {
+
+  })
+
+  // Get user
+  server.get('user/:id', (req, res, next) => {
+    let id = req.params.id
   })
 }
